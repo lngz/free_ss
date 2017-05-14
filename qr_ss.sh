c990ffc146daa2be8a91df8d0ddc511db1e9bfbc
shizl <shizl@shizldeiMac.lan>
2017-05-14 19:40:34 +0800

    update

--- qr_ss.sh
+++ qr_ss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-qrcode=`zbarimg -q https://www.shadowsocks8.biz/images/server0$1.png`
+qrcode=`zbarimg -q http://get.shadowsocks8.cc/images/server0$1.png`
 echo $qrcode
 python ss2.py $qrcode $2
 
